19935b7b2b7d3af2973f35aee73933267a537672
Laurence Sadler <sirlaurence@protonmail.com>
2023-01-04 05:51:44 -0800

    [ADDED]Run and Gun automation
    - Ability to move forward and shooting enemies
    - Updated readme with some notes
    - .jar for Sikulix

--- MetalSlugTest.py
+++ MetalSlugTest.py
@@ -1,7 +1,6 @@
 # wait x seconds for menu screen - then click to skip intro
 # click main mission -> arcade -> one player
 # move and shoot left
-
 gamePath="C:\GOG Games\Metal Slug\mslug1.exe"
 openApp(gamePath)
 wait(10)
@@ -9,7 +8,20 @@ click()#skips intro
 for i in range(3):
     wait(1.25)
     type(Key.ENTER)
+wait(5)
+#in-game
+seconds = 0
+timeStart = time.time()
+while seconds < 30.0:
+   keyDown(Key.RIGHT) # run and shoot    
+   type(Key.CTRL)
+   seconds += round(time.time() - timeStart,0)
+   print(seconds)
 
-wait(10)
-keyDown(Key.ALT + Key.F4)
+#exit game
+type(Key.ESC)
+wait(5)
+keyDown(Key.ALT + Key.F4) 
+exit()
+
 
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # SikuliX-Sandbox
- 
+
+Command to Run: 
+```
+java -jar .\sikulixidewin-2.0.6-20220817.114540-15.jar -r .\MetalSlugTest.py
+```
+
+Notes:
+- What i found out was that running through the IDE for the first time will result in smooth run, but sequential runs after that 'keyDown' command would not work. Temporary work around is to run the full command through the terminal
+- After running the automation, for some reason it doesn't return the keyboard input right away....its annoying
+- keyDown input doesn't show in the log output
+
